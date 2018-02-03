ac21ece247bf036e7fe37c3b0b0b6477bdcf3821
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:51 +0000

    Only favorite user can access show page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  before_action :current_user_must_be_favorite_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_favorite_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_favorite_user
     favorite = Favorite.find(params[:id])
