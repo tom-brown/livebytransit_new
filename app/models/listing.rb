34fdbb73f2d571680f224dc6d25831d99ae0e544
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:10:44 +0000

    Direct association between listings and listings_searches

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :listings_searches,
+             :dependent => :destroy
+
   has_many   :showings,
              :dependent => :destroy
 
--- app/models/listings_search.rb
+++ app/models/listings_search.rb
@@ -1,6 +1,8 @@
 class ListingsSearch < ApplicationRecord
   # Direct associations
 
+  belongs_to :listing
+
   # Indirect associations
 
   # Validations
