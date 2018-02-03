5d3840bebfe975ad463232d9aad56b3adb727def
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:11:46 +0000

    Whitelist columns in CtaStation for ActiveAdmin

--- app/admin/cta_station.rb
+++ app/admin/cta_station.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CtaStation do
 
+ permit_params :name, :lat, :lng, :address
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
