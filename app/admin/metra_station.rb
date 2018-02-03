609500f9279de69781d24cae13b97570765138dd
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:11:36 +0000

    Whitelist columns in MetraStation for ActiveAdmin

--- app/admin/metra_station.rb
+++ app/admin/metra_station.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MetraStation do
 
+ permit_params :lat, :lng
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
