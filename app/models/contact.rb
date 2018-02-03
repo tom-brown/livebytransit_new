61969b954ced179c4678fe740b9035736c995156
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:10:44 +0000

    Direct association between users and contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :contacts,
+             :dependent => :destroy
+
   has_many   :showings,
              :dependent => :destroy
 
