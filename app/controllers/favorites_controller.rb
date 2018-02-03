742f0f563e3e0baa60f3c7e2cf73b714dfe833bf
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:51 +0000

    Display only signed in users favorites on index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,7 +10,7 @@ class FavoritesController < ApplicationController
   end
 
   def index
-    @favorites = Favorite.all
+    @favorites = current_user.favorites.page(params[:page]).per(10)
 
     render("favorites/index.html.erb")
   end
