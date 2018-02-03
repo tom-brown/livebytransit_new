b4aaa3de84c05e88238bd99d664ab293798b6547
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:51 +0000

    Only search user can modify search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,14 @@
 class SearchesController < ApplicationController
+  before_action :current_user_must_be_search_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_search_user
+    search = Search.find(params[:id])
+
+    unless current_user == search.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @searches = Search.all
 
