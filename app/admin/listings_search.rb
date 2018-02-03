58a41f05d96b3bed27dec8a1c7e303a3ca518d70
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:51 +0000

    Whitelist columns in ListingsSearch for ActiveAdmin

--- app/admin/listings_search.rb
+++ app/admin/listings_search.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ListingsSearch do
 
+ permit_params :listing_id, :search_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
