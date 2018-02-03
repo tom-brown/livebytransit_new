c22673163defb8534c8b8240340ada4f3cd32584
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:11:16 +0000

    Whitelist columns in Listing for ActiveAdmin

--- app/admin/listing.rb
+++ app/admin/listing.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Listing do
 
+ permit_params :address, :lat, :lng, :user_id, :beds, :baths, :half_baths, :description, :garage_spaces, :assigned_parking, :active, :age, :size, :city_id, :heat_gas, :heat_radiator, :ac_window_units, :ac_central, :list_date, :pin, :recent_rehab, :vintage, :new_construction, :price, :rental, :state, :st_num, :street, :st_suffix, :cp, :unit, :zip_code, :property_type, :laundry, :cats, :dogs, :available_date, :image_urls, :neighborhood_id, :picture_count, :mls_id, :lo_mlsid, :la_first_name, :la_last_name, :total_baths
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
