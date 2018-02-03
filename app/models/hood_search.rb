87cc5e5a8430504e5babd3924aafd750816eecbf
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:10:44 +0000

    Direct association between neighborhoods and hood_searches

--- app/models/hood_search.rb
+++ app/models/hood_search.rb
@@ -1,6 +1,8 @@
 class HoodSearch < ApplicationRecord
   # Direct associations
 
+  belongs_to :neighborhood
+
   belongs_to :search
 
   # Indirect associations
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,9 @@
 class Neighborhood < ApplicationRecord
   # Direct associations
 
+  has_many   :hood_searches,
+             :dependent => :destroy
+
   belongs_to :city
 
   # Indirect associations
