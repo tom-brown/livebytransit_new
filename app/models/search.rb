0260f1981d05f78bdfd97183c3e580f4486185c5
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:10:44 +0000

    Direct association between searches and hood_searches

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,9 @@
 class Search < ApplicationRecord
   # Direct associations
 
+  has_many   :hood_searches,
+             :dependent => :destroy
+
   has_many   :metra_searches,
              :dependent => :destroy
 
