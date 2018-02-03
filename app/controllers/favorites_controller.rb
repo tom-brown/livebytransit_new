59cbaff09b9b590a1167e779d2124dba6f7ba08e
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:51 +0000

    Automatically associate comment to signed in user

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -47,8 +47,6 @@ class FavoritesController < ApplicationController
 
   def update
     @favorite = Favorite.find(params[:id])
-
-    @favorite.user_id = params[:user_id]
     @favorite.listing_id = params[:listing_id]
 
     save_status = @favorite.save
