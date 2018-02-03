eb6e0db1044055c943fe7d2dca9e8a604cf96873
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:46 +0000

    Whitelist columns in HoodSearch for ActiveAdmin

--- app/admin/hood_search.rb
+++ app/admin/hood_search.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register HoodSearch do
 
+ permit_params :neighborhood_id, :search_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
