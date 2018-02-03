b9661cd61c4144edd22ac25c94957c6441fb21fc
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:28 +0000

    Whitelist columns in CtaAssign for ActiveAdmin

--- app/admin/cta_assign.rb
+++ app/admin/cta_assign.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CtaAssign do
 
+ permit_params :cta_station_id, :cta_line_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
