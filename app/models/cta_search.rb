ac8a1af364881e4f415278bfd93a7db5500e6f23
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:10:44 +0000

    Direct association between searches and cta_searches

--- app/models/cta_search.rb
+++ app/models/cta_search.rb
@@ -1,6 +1,8 @@
 class CtaSearch < ApplicationRecord
   # Direct associations
 
+  belongs_to :search
+
   # Indirect associations
 
   # Validations
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,9 @@
 class Search < ApplicationRecord
   # Direct associations
 
+  has_many   :cta_searches,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
