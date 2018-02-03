feb8e552e50404cf92a52dcb3c1c4ccdbc06d3a8
Raghu Betina <raghu@firstdraft.com>
2018-02-03 15:12:42 +0000

    Whitelist columns in CitySearch for ActiveAdmin

--- app/admin/city_search.rb
+++ app/admin/city_search.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CitySearch do
 
+ permit_params :search_id, :city_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
